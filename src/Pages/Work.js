e43053378cf5d57db3fef61c3357fb78f203769a
abdurrahmanALONG <abdullahhanxin@gmail.com>
2022-06-11 20:18:33 +0800

    padding problem solve

--- src/Pages/Work.js
+++ src/Pages/Work.js
@@ -7,7 +7,7 @@ import project4 from '../logos/pro-4-1.jpg';
 const Work = () => {
 
     return (
-        <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        <div name='work' className='w-full md:h-screen pb-20 text-gray-300 bg-[#0a192f]'>
             <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
                 <div className='pb-8'>
                     <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
@@ -17,7 +17,7 @@ const Work = () => {
                 </div>
 
                 {/* Container */}
-                <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                <div className='grid  sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
                     {/* Grid Item */}
                     <div
